f0346b187418bce8d9a3c317fd4e85841b6e03b3
GabriellaQiong <qiong@seas.upenn.edu>
2014-04-19 12:01:35 -0400

    add deploy file

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp -i bashrc ~/.bashrc
+cp -i bash_local ~/.bash_local
+cp -i bash_aliases ~/.bash_aliases
+cp -i bash_functions ~/.bash_functions
+cp -i vimrc ~/.vimrc
+cp -i tmux.conf ~/.tmux.conf
+cp -i tmuxline ~/.tmuxline
